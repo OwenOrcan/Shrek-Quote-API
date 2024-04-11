01dea5d852f2c923832583880a6ca6cbeac7bc86
OwenOrcan <owenorcan@gmail.com>
2024-04-11 16:32:10 -0400

    test

--- django_project/settings.py
+++ django_project/settings.py
@@ -81,6 +81,7 @@ DATABASES = {
     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
+
 """DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
