b2430e55e8144364b4836515ede67f61ee3aeb24
OwenOrcan <owenorcan@gmail.com>
2024-04-11 17:24:17 -0400

    db

--- shrek_api/views.py
+++ shrek_api/views.py
@@ -25,3 +25,13 @@ def add_quote(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def put_all_data(request):
+    if request.method == 'POST':
+        serializer = QuoteSerializer(data=request.data, many=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
