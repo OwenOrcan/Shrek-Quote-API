ce1bb98245a116747c7d7dbb48a0e9eee0c9abc5
OwenOrcan <owenorcan@gmail.com>
2024-04-11 17:25:20 -0400

    url

--- shrek_api/urls.py
+++ shrek_api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import random_quote,add_quote
+from .views import random_quote,add_quote, put_all_data
 urlpatterns = [
     # Your other URLs...
     path('random-quote/', random_quote, name='random-quote'),
     path('add-quote/', add_quote, name='add-quote'),
+    path('put-all-data/', put_all_data, name='put-all-data'),
 ]
